4bd74ae183863961a9a8a51baddfc69043390a90
Nabila Jahan Mridula <nabilabaiust97@gmail.com>
2026-01-21 01:36:56 +0600

    Update problemJ.c

---
+++ sheet7(Recursion)/problemJ.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ 
+void num(int i,int n,long long multi)
+{
+   if(i>n)
+   {
+       printf("%lld",multi);
+   }
+   else{
+    num(i+1, n, multi*i);
+ 
+   }
+ 
+}
+int main(){
+int n;
+scanf("%d", &n);
+num(1,n,1);
+}
